064d2daab13f947ca3d846a56c0257f34317e7e6
hana-nazer <hanapp9966@gmail.com>
2023-05-26 11:51:06 +0530

    made changes in c5.module.css

--- src/components/C4.js
+++ src/components/C4.js
@@ -1,17 +1,16 @@
 import styles from "../stylesheets/c4.module.css";
 const C4 = () => {
   return (
-    <div className={styles.c4}>
-       <div className={styles.gridBackground}></div>
-      <div className={styles.c4Inner} />
+    <div className={styles.main_div}>
+      <div className={styles.gridBackground}></div>
+      <div className={styles.inner_div} />
       <div className={styles.button}>
         <div className={styles.buttonChild} />
-        <b className={styles.buildYourLegacy}>Get Started</b>
+        <b className={styles.get_button}>Get Started</b>
       </div>
-      <img className={styles.maskGroupIcon1} alt="" />
       <div className={styles.getAnEstimateParent}>
         <div className={styles.getAnEstimate}>get an estimate</div>
-        <div className={styles.groupWrapper}>
+        <div className={styles.wrapper_div}>
           <div className={styles.tryOurCostCalculatorParent}>
             <div className={styles.tryOurCost}>TRY OUR COST CALCULATOR</div>
           </div>
--- src/components/C5.js
+++ src/components/C5.js
@@ -1,28 +1,28 @@
 import styles from "../stylesheets/c5.module.css";
 const C5 = () => {
   return (
-    <div className={styles.c5}>
+    <div className={styles.main_div}>
       <div className={styles.our5PillarsApproachWrapper}>
         <b className={styles.our5PillarsContainer}>
           <p className={styles.our5Pillars}>OUR 5 PILLARS</p>
           <p className={styles.our5Pillars}>APPROACH</p>
         </b>
       </div>
-      <div className={styles.ewrParent}>
-        <div className={styles.ewr}>
-          <div className={styles.divapproacheditoslideshowInn}>
+      <div className={styles.subDiv_parent}>
+        <div className={styles.sub_div}>
+          <div className={styles.inner_div}>
             <div className={styles.svg}>
               <div className={styles.frame}>
-                <div className={styles.ins}>
-                  <b className={styles.instinctive}>ORIGINAL</b>
+                <div className={styles.text_div}>
+                  <b className={styles.text}>ORIGINAL</b>
                 </div>
-                <img className={styles.groupIcon} alt="" src="/group1.svg" />
+                <img className={styles.Icon1} alt="" src="/group1.svg" />
               </div>
             </div>
           </div>
         </div>
-        <div className={styles.ewr}>
-          <div className={styles.divapproacheditoslideshowInn}>
+        <div className={styles.sub_div}>
+          <div className={styles.inner_div}>
             <div className={styles.svg}>
               <div className={styles.frame}>
                 <img
@@ -33,7 +33,7 @@ const C5 = () => {
                 <img className={styles.frameChild} alt="" src="/group-11.svg" />
                 <img className={styles.frameItem} alt="" src="/group-21.svg" />
                 <div className={styles.ins1}>
-                  <b className={styles.instinctive}>INSTINCTIVE</b>
+                  <b className={styles.text}>INSTINCTIVE</b>
                 </div>
               </div>
             </div>
